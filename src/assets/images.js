71baa3cd24bb612d5808392bfe19420f81bc2f98
Eloissa <127953033+mselletoe@users.noreply.github.com>
2025-05-04 14:07:53 +0800

    "fix: routing"

---
+++ src/assets/images.js
@@ -0,0 +1,10 @@
+// src/assets/images.js
+
+import cocheLogo from './coche-logo.svg';
+import sideimage from './image-3.png';
+// add other images as needed
+
+export {
+  cocheLogo,
+  sideimage
+};
