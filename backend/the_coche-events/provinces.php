cc9694fe1d26f37a5a217ce8758d236e06030816
Eloissa <127953033+mselletoe@users.noreply.github.com>
2025-06-02 17:52:13 +0800

    update backend files

--- backend/the_coche-events/provinces.php
+++ backend/the_coche-events/provinces.php
@@ -1,7 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, ngrok-skip-browser-warning");
+header("Access-Control-Max-Age: 600");
 
 // Handle preflight request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
