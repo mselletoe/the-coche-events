46235d26af4bcf6a1e2763210512b10e24aa5639
Eloissa <127953033+mselletoe@users.noreply.github.com>
2025-07-07 21:16:25 +0800

    media query and mobile

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://1467-2001-fd8-1eaf-fa54-6cc4-b1ae-9583-61ab.ngrok-free.app//the_coche-events', // fixed double slash
+  baseURL: 'https://65f1-1-37-83-4.ngrok-free.app//the_coche-events', // fixed double slash
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
